12885abe481435eb33a17923ff8f7363233dc006
SFonderie <sydney3449@gmail.com>
2023-05-23 14:05:08 -0700

    Tile Data Base
    Added basic tile data asset with properties but no functions.

--- Iota/Source/IotaCore/Private/Tileset/TileDataAsset.cpp
+++ Iota/Source/IotaCore/Private/Tileset/TileDataAsset.cpp
@@ -1,5 +1,15 @@
 // Copyright Sydney Fonderie, 2023. All Rights Reserved.
 
 #include "Tileset/TileDataAsset.h"
+#include "Tileset/TileBound.h"
+#include "Tileset/TilePortal.h"
+#include "Engine/World.h"
 
+#include "UObject/ObjectSaveContext.h"
 
+DEFINE_LOG_CATEGORY_CLASS(UTileDataAsset, LogTileData);
+
+void UTileDataAsset::PreSave(FObjectPreSaveContext ObjectSaveContext)
+{
+	Super::PreSave(ObjectSaveContext);
+}
--- Iota/Source/IotaCore/Public/Tileset/TileDataAsset.h
+++ Iota/Source/IotaCore/Public/Tileset/TileDataAsset.h
@@ -6,9 +6,77 @@
 #include "Engine/DataAsset.h"
 #include "TileDataAsset.generated.h"
 
-/**  */
+/** Defines different tile functions within a tileset. */
+UENUM(BlueprintType)
+enum class ETileScheme : uint8
+{
+	/**
+	 * Indicates that the tile should be used as the level start point.
+	 * Start tiles must have at least one portal.
+	 */
+	Start,
+
+	/**
+	 * Indicates that the tile should be used to connect other major tiles.
+	 * Connector tiles must have at least two portals.
+	 */
+	Connector,
+
+	/**
+	 * Indicates that the tile should be used as a major branch or arena.
+	 * Intermediate tiles must have at least three portals.
+	 */
+	Intermediate,
+
+	/**
+	 * Indicates that the tile contains some kind of level objective.
+	 * Objective tiles must have at least two portals.
+	 */
+	Objective,
+
+	/**
+	 * Indicates that the tile should be used to close-off empty portals.
+	 * Stopper tiles can have at most one portal.
+	 */
+	Stopper,
+
+	/**
+	 * Indicates that the tile should be used as the level exit point.
+	 * Exit tiles must have at least one portal.
+	 */
+	Exit,
+};
+
+/** Container describing a tile. */
 UCLASS()
 class IOTACORE_API UTileDataAsset : public UDataAsset
 {
 	GENERATED_BODY()
+
+public:
+
+	/** Class logger used to warn users about data overrides. */
+	DECLARE_LOG_CATEGORY_CLASS(LogTileData, Log, All);
+
+	/** Pointer to the actual tile level instance. */
+	UPROPERTY(Category = "Level", EditAnywhere)
+	TSoftObjectPtr<class UWorld> Level;
+
+	/** Preferred tile scheme. */
+	UPROPERTY(Category = "Level", EditAnywhere)
+	ETileScheme Scheme = ETileScheme::Stopper;
+
+	/** A list of the tile's exit portals. */
+	UPROPERTY(Category = "Description", EditAnywhere)
+	TArray<struct FTilePortal> Portals;
+
+	/** A list of the tile's summary collision bounds. */
+	UPROPERTY(Category = "Description", EditAnywhere)
+	TArray<struct FTileBound> Bounds;
+
+	/** 
+	 * Invoked just before the data asset is saved. Tile Data Asset uses this method to enforce
+	 * data rules, automatically fixing violations and printing the results to the tile data log.
+	 */
+	virtual void PreSave(FObjectPreSaveContext ObjectSaveContext) override;
 };
